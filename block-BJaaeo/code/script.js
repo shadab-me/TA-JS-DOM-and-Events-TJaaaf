0fe2cb4f21f4f8d3efddae75ee61bd967674d26d
Shadab Ali <shadabali604@gmail.com>
2020-09-17 00:56:47 +0530

    added: completed

---
+++ block-BJaaeo/code/script.js
@@ -0,0 +1,26 @@
+let input = document.querySelector('.inputs');
+let output = document.querySelector('.output');
+let result = document.querySelector('.result');
+function handler(event){
+if(event.target.hasAttribute('data-number')){
+     output.innerText = output.innerText + event.target.dataset.number
+}
+if(event.target.hasAttribute('data-operation')){
+    output.innerText = output.innerText + event.target.dataset.operation;
+}
+console.log(event.target.classList.contains("result"));
+if(event.target.classList.contains("result")){
+    console.log(`(${output.innerText})`);
+     output.innerText = `${eval(output.innerText)}`
+     console.log(output.innerText);
+
+}
+if(event.target.classList.contains("clear")){
+    output.innerText = ''
+}
+}
+
+
+
+
+input.addEventListener('click', handler);
