474c3cf50fbd515a93efa80aa5d576d4f487af4d
Shadab Ali <shadabali604@gmail.com>
2020-09-22 12:46:06 +0530

    added: completed

--- block-BJaaep/code/script.js
+++ block-BJaaep/code/script.js
@@ -1,9 +1,33 @@
-let boxes = document.querySelector('.boxes');
-console.log(boxes);
+let boxes = document.querySelectorAll('.boxes');
+ console.log(boxes);
 
-function handler(event ){
-    
-   event.target.innerText = '5';
+function handler(event){
+  let value = event.target.dataset.index;
+ event.target.classList.add('flip')
+
+ event.target.innerHTML= `<h1>${Number(value)+1}</h1>`;
+ setTimeout(
+    function(){
+       event.target.classList.remove('flip');
+       event.target.innerHTML =''
+    }, 5000)
+ }
+
+ [...boxes[0].children].forEach((box, index) => {
+   box.setAttribute('data-index', index);
+   box.addEventListener('click', handler);
+
+   console.log(index)
+
+});
+// with event
+function wHandler(event){
+   let value = event.target.dataset.index;
+if(event.target.hasAttribute('data-index')){
+let value = event.target.dataset.index;
+event.target.classList.add('flip')
+event.target.innerHTML= `<h1>${Number(value)+1}</h1>`;
 }
-
-boxes.addEventListener('click', handler);
+}
+console.log(boxes[1]);
+ boxes[1].addEventListener('click', wHandler)
