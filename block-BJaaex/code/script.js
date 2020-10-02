1ba8cac6f2da3516e6f9df56693b39f9b0932add
Shadab Ali <shadabali604@gmail.com>
2020-10-02 16:00:59 +0530

    added: timer left

--- block-BJaaex/code/script.js
+++ block-BJaaex/code/script.js
@@ -1,52 +1,103 @@
 let contain = document.querySelector('.container');
+let move = document.querySelector('#move');
+let t = document.querySelector('#time');
+
+let m = 0;
+let timer = 0;
+function time(){
+setInterval(function(){
+     timer++
+     t.innerText = timer;
+    }, 1000)
 
 
+}
+  
 
 let data = [1, 2, 3, 4, 5, 6, 7, 8];
+
 let counter = 0;
-let value  = [];
-function createUi(data){
-   data.forEach(el => {
-       let box  = document.createElement('div');
-       box.setAttribute('class', 'box');
-       box.setAttribute('id', 'hide')
-       let i = document.createElement('i');
-       i.innerText = el
-       i.setAttribute('data-in', el)
-       i.setAttribute('class', 'hidden')
-       box.appendChild(i);
-       contain.appendChild(box)    
+let Value = [];
+function createUi(data) {
+     data.forEach(el => {
+        let box = document.createElement('div');
+        box.setAttribute('class', 'box');
+        box.setAttribute('id', 'hide')
+        let i = document.createElement('i');
+        i.innerText = el
+        i.setAttribute('data-in', el)
+        i.setAttribute('class', 'hidden')
+        box.appendChild(i);
+        contain.appendChild(box)
     });
 }
-function handler(event){
-     if(event.target.id == "hide"){
-        let first = (event.target.childNodes[0].dataset.in);
-         value.push(first)
-         counter++
-      i.classList.remove('hidden')   
-    } 
-     if(event.target.hasAttribute('data-in')){
-         let second = (event.target.dataset.in);
-         value.push(second);
-         counter++
-     }
 
-     if(counter == 2){
-    console.log(value)
-    Match(value);
-    counter = 0
-     value = [];
-     }
-   
+function handler(event) {
+    if (event.target.id == "hide") {
+        m = m+1
+        let el = event.target.childNodes[0];
+        Value.push(el.dataset.in);
+        counter++
+        el.classList.remove('hidden');
+        event.target.classList.add('selected');
+    }
+    if (event.target.hasAttribute('data-in')) {
+        m = m+1
+        let el = event.target;
+        Value.push(el.dataset.in);
+        el.classList.remove('hidden')
+        el.parentElement.classList.add('selected')
+        counter++
+    }
+
+     con(counter, event);
+     move.innerText = m
+     
+
 }
-function Match(arr){
-   if(arr[0] == arr[1]){
-       console.log('Super Match')
-   }
-   else{
-       console.log("Not Match")
-   }
-} 
+
+function con(count){
+    if(counter == 2){
+     let Selected  = document.querySelectorAll('.selected');
+     console.log(Selected);
+     let hidden = document.querySelectorAll('#hide');
+     [...hidden].forEach((item) => {
+          item.classList.add('evenRemover');
+      });
+
+      if(Value[0] == Value[1]){
+          [...Selected].forEach(item => {
+              item.classList.add('match');
+              item.classList.remove('selected');
+              item.classList.add('evenRemover');
+            });
+          [...hidden].forEach((item) => {
+            item.classList.remove('evenRemover');
+        });
+
+      }else if(Value[0] != Value[1]){
+         [...Selected].forEach(item => item.classList.add('notMatch'));
+        setTimeout(function(){
+            [...Selected].forEach(item => {
+                item.classList.remove('notMatch');
+                item.classList.remove('selected');
+                item.childNodes[0].classList.add('hidden');
+            });
+        }, 2000);
+
+        [...hidden].forEach((item) => {
+            item.classList.remove('evenRemover');
+        });
+      }
+
+    Value = [];
+    counter = 0;
+    }
+}
+   
+
+
+
 contain.addEventListener('click', handler)
 console.log(counter);
 createUi(data.concat(data));
